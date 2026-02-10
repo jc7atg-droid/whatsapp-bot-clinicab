6d731628568ecf6b9be55b55991b687a1e9bae4f
Juan Camilo <jc7at@icloud.com>
2026-02-10 00:51:15 -0500

    Fix auth path

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ async function transcribeAudio(audioBuffer) {
 
 async function startBot() {
 
-  const { state, saveCreds } = await useMultiFileAuthState("./auth" + Date.now())
+  const { state, saveCreds } = await useMultiFileAuthState("./auth")
   const sock = makeWASocket({ auth: state })
 
   sock.ev.on("creds.update", saveCreds)
