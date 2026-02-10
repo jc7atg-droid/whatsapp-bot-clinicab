d2442b7234930e275bff26b189c3103d20ba6832
Juan Camilo <jc7at@icloud.com>
2026-02-10 01:01:59 -0500

    Fix duplicate http

--- index.js
+++ index.js
@@ -1464,11 +1464,3 @@ server.listen(PORT, () => {
 });
 
 startBot()
-
-// Keep alive HTTP server
-http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('Bot running');
-}).listen(process.env.PORT || 8080, () => {
-  console.log('HTTP server running');
-});
